56586a2b85c5379298cf4fbd84c193cf2dc8f0f4
404-Vladimir <vmusatpv23@gmail.com>
2026-09-25 14:05:13 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,26 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
-android { namespace = "com.flowesal.vpn"; compileSdk = 35
-    defaultConfig { applicationId = "com.flowesal.vpn"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.flowesal.vpn"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.flowesal.vpn"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
